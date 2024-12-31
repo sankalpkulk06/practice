c4e9385267340a0a35b097e4d3e27408660fb2b2
sankalpkulk06 <sankalpkulk06@gmail.com>
2024-12-30 16:23:06 -0800

    practice insertion sort

--- Sort/InsertionSort/practice.py
+++ Sort/InsertionSort/practice.py
@@ -1,14 +1,18 @@
 
-
+# 1. 30th December, 
 def insertion_sort(arr):
 
     for i in range(1, len(arr)):
         j = i   # start looping back from ith postion
 
+        # while j-1 in bounds
         while j > 0:
-            if arr[j-1] > arr[j]:
-                arr[j-1], arr[j] = arr[j], arr[j-1]
-            j = j - 1
+
+            # if right element is greater than left element, if yes then swap.
+            if arr[j-1] > arr[j]:  
+                arr[j-1], arr[j] = arr[j], arr[j-1]     # swap
+                
+            j = j - 1   # traverse backwards
     return arr
 
 
