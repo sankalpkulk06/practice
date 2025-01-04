5068343d693d1f302201ce7d78768be1fabc794a
sankalpkulk06 <sankalpkulk06@gmail.com>
2025-01-03 16:31:55 -0800

    3rd Jan bubble sort practice

--- Sort/BubbleSort/practice.py
+++ Sort/BubbleSort/practice.py
@@ -1,15 +1,26 @@
-
-
-
-# 1. 30th December, 2024
+# 2. 3rd Jan, 2025
 def bubble_sort(arr):
-
+    
     for i in range(len(arr)):
-
         for j in range(len(arr)-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+    
     return arr
 
 list = [3,4,2,1,5,9,7,8,6]
 print(bubble_sort(list))
+
+
+# 1. 30th December, 2024
+# def bubble_sort(arr):
+
+#     for i in range(len(arr)):
+
+#         for j in range(len(arr)-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#     return arr
+
+# list = [3,4,2,1,5,9,7,8,6]
+# print(bubble_sort(list))
