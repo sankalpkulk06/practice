47bc33f744b62072feb90f129ef1c07245cfd764
sankalpkulk06 <sankalpkulk06@gmail.com>
2025-01-20 11:18:43 -0800

    solved 119

--- Leetcode/Easy/119_pascal_2.py
+++ Leetcode/Easy/119_pascal_2.py
@@ -5,6 +5,16 @@ return kth row
 """
 
 def getRow(rowIndex):
-    return
+
+    triangle = []
+
+    for i in range(rowIndex+1):
+        row = [1] * (i+1)
+
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        
+        triangle.append(row)
+    return triangle[rowIndex]
 
 print(getRow(3))
