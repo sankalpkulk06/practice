e074b4f8da7a2cb776f9386918cb8d577ee51d2d
Sankalp Kulkarni <sanka@MacBook-Pro.local>
2026-01-06 15:26:35 -0500

    revisited: LC 1004

--- Patterns/TwoPointers/Type3/VariableSize/1_1004_max_consec_ones_3.py
+++ Patterns/TwoPointers/Type3/VariableSize/1_1004_max_consec_ones_3.py
@@ -56,3 +56,42 @@ print(res4)
 nums5 = [0,0,0,0,0]
 res5 = maxConsecutiveOnes(nums5, 3)
 print(res5)
+
+"""
+Pattern: variable sliding window
+
+eg. nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+can flip only 2 zeros
+
+[1,1,1], zeros = 0, len = 3
+[1,1,1,0], zeros = 1, len = 4
+[1,1,1,0,0], zeros = 2, len = 5
+[1,1,1,0,0,0], zeros = 3, len = 6 (INVALID)
+shift L till zeros is less than k
+[1,1,0,0,0] (INVALID)
+[1,0,0,0] (INVALID)
+...
+[0,0], zeros = 2, len = 2
+[0,0,1], zeros = 2, len = 3
+...
+[0,0,1,1,1,1], zeros = 2, len = 6
+[0,0,1,1,1,1,0], zeros = 3, len = 7 (INVALID)
+shift L till zeros is less than k
+[0,1,1,1,1,0], zeros = 2, len = 6
+
+ANS = 6
+
+Thought process:
+- init zeros (which stores the num of zeros in window), and max_len (stores the max length of window)
+- init L, R = 0, 0
+- while R < len(nums):
+    - if the incoming num (R) is 0: then zeros += 1
+    - if the number of zeros is GT k, we need to move L pointer till zeros is LTE k. so, while zeros > k:
+        - if left (exiting) num is 0: then decrement zeros
+        - increment L pointer
+
+    - max_len = max(max_len, R - L + 1)
+    - R += 1
+
+- return max_len
+"""
