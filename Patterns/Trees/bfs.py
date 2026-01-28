59146d79d3ef3cf849dfcd5d46a8335a5e174646
Sankalp Kulkarni <sanka@Mac.lan>
2026-01-28 10:10:11 -0800

    practice: bfs

---
+++ Patterns/Trees/bfs.py
@@ -0,0 +1,34 @@
+"""
+- BFS from a Given Source of Undirected Tree
+- Uses a Queue
+"""
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def iterative_bfs(self, root):
+        if root is None:
+            return
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            print(node.val, end=" ")
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    print("Iterative BFS:")
+    root.iterative_bfs(root)
